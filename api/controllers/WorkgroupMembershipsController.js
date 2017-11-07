9c8e98f54b76f1f0ee27de1db3199878c573167e
RyanQuey <rlquey2@Gmail.com>
2017-11-06 17:12:10 -0800

    squash this

--- api/controllers/WorkgroupMembershipsController.js
+++ api/controllers/WorkgroupMembershipsController.js
@@ -4,7 +4,6 @@
  * @description :: Server-side logic for managing Workgroupmemberships
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-console.log(sails.model);
 module.exports = {
 
 };
