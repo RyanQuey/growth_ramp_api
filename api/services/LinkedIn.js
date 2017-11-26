2675fd8170560aeb6679b29a002597a398fba73f
RyanQuey <rlquey2@Gmail.com>
2017-11-25 20:41:42 -0800

    successfully post to LinkedIn

--- api/services/LinkedIn.js
+++ api/services/LinkedIn.js
@@ -1,14 +1,23 @@
 //just doing rest for LinkedIn, SDK doesn't support mobile and no library looks good enough to have another dependency
 //this also gives me greater flexibility
+const LIApi = "https://api.linkedin.com"
+const _setup = (account) => {
+  const axiosLI = axios.create({
+      headers: {
+        'x-li-format': 'json',
+        'Authorization': `Bearer ${account.accessToken}`
+      }
+  })
 
-const axios = require('axios')
-//might create sep lib for li if need to use this stuff elsewhere
-axios.defaults.baseUrl = "https://api.linkedin.com/v1"
-axios.defaults.headers.common['x-li-format'] = 'json'
+  return axiosLI
+}
 
 const LinkedIn = {
   createPost: (account, post, utms) => {
     return new Promise((resolve, reject) => {
+      const axiosLI = _setup(account)
+      //might create sep lib for li if need to use this stuff elsewhere
+
 //if was going to supply all of these; but we'd rather they get them from teh post itself
 /*      const body = {
         "comment": `${post.text} , //"Check out developer.linkedin.com!", //this is the main post body.
@@ -24,17 +33,18 @@ const LinkedIn = {
       }*/
 
       const body = {
-        "comment": `${post.text} ${post.contentUrl}?${utms}`, //"Check out developer.linkedin.com!", //this is the main post body.
+        "comment": post.text, //"Check out developer.linkedin.com!", //this is the main post body.
         //not sure if this will get sent...
         "content": {
-          "submitted-image-url": post.uploadedContent[0].url
+          "submitted-image-url": post.uploadedContent[0].url,
+          "submitted-url": `${post.contentUrl}?${utms}`, //other urls can be in the comment, but LI will only analyze the first one for content to share
         },
         "visibility": {
           "code": post.visibility || "connections-only" //"anyone" or "connections-only"
         }
       }
 
-      LinkedIn[post.channel](body)
+      LinkedIn[post.channel](body, axiosLI)
       .then((response) => {
         const {updateKey, updateUrl} = response
         //perhaps persist these if we want the user to be able to look at the link or update it
@@ -48,17 +58,13 @@ const LinkedIn = {
     })
   },
 
-  PERSONAL_POST: (body) => {
-    const path = `/people/~/shares?format=json`
-
-    return axios.post(path, body)
+  PERSONAL_POST: (body, axiosLI) => {
+    return axiosLI.post(`${LIApi}/v1/people/~/shares?format=json`, body)
   },
 
 //TODO set to page...
-  PAGE_POST: (body) => {
-    const path = `/people/~/shares?format=json`
-
-    return axios.post(path, body)
+  PAGE_POST: (body, axiosLI) => {
+    return axiosLI.post(`${LIApi}/v1/people/~/shares?format=json`, body)
   },
 
 //TODO set to group...
