bab56b4a8081bb480f17d2b6dee99931609b0066
RyanQuey <rlquey2@Gmail.com>
2018-01-06 09:32:59 -0800

    do not publish delayed posts of unpaid users

--- backgroundJobs/publish_delayed_posts.js
+++ backgroundJobs/publish_delayed_posts.js
@@ -21,6 +21,9 @@ module.exports = class PublishDelayedPosts extends Job {
     this.running = true;
     const now = moment().format()
 
+    const campaignsToPublish = {}
+    const usersWithFailedPublishes = []
+
     Posts.find({
       publishedAt: null,
       delayedUntil: {
@@ -32,9 +35,13 @@ module.exports = class PublishDelayedPosts extends Job {
 sails.log.debug(postsToPublish, now);
       //publish by campaign; that is how the functions are already written, gets the accounts in a systematic way, makes sure to update campaign at the end, etc
 
-      //find unique campaigns
-      const campaignsToPublish = {}
+      if (!postsToPublish || !postsToPublish.length) {
+        return []
+      }
 
+      const uniqueUserIds = []
+
+      //find unique campaigns
       for (let post of postsToPublish) {
         let campaign = post.campaignId
         //get it the same as how it would be if publishing post after pressing publish campaign button
@@ -46,15 +53,62 @@ sails.log.debug(postsToPublish, now);
         } else {
           campaignsToPublish[campaign.id].readyPosts.push(post)
         }
+
+        if (!uniqueUserIds.includes(post.userId)) {
+          uniqueUserIds.push(post.userId)
+        }
       }
 
+      return Users.find({id: uniqueUserIds})
+    })
+    .then((users) => {
+      if (!users || !users.length) {
+        return []
+      }
+
+      //copies regular posting flow
+      const promises = users.map((user) => {
+        return new Promise((resolve, reject) => {
+          AccountSubscriptions.checkStripeStatus(user.id)
+          .then((sub) => {
+            if (!ALLOWED_EMAILS.includes(user.email) && (!sub || ["past_due", "canceled", "unpaid", null].includes(sub.subscriptionStatus))) {
+              usersWithFailedPublishes.push({
+                user: user,
+                error: {message: "Payment is required before user can publish", code: "delinquent-payment"},
+              })
+
+              return resolve({userId: user.id, status: "rejected"} )
+            } else {
+              return resolve({userId: user.id, status: "accepted"})
+            }
+
+          })
+          .catch((err) => {
+            sails.log.debug("Failure checking stripe while posting delayed post for ", user.email);
+            sails.log.debug(err);
+
+            return resolve({userId: user.id, status: "rejected"} )
+          })
+
+        })
+
+      })
+    })
+    .then((results) => {
+      const approvedUserIds = results.filter((r) => r.status === "accepted" && r.userId)
+
       const campaignIds = Object.keys(campaignsToPublish)
-      const promises = campaignIds.map((id) => {
+      const approvedCampaignIds = campaignIds.filter((id) => {
+        let campaignUserId = campaignsToPublish[id].campaign.userId
+        return approvedUserIds.includes(campaignUserId)
+      })
+
+      const promises2 = approvedCampaignIds.map((id) => {
         const data = campaignsToPublish[id]
         return Campaigns.publishCampaign(data.campaign, data.readyPosts)
       })
 
-      return Promise.all(promises)
+      return Promise.all(promises2)
     })
     .then((allResults) => {
       console.log("Finished publishing delayed posts:");
