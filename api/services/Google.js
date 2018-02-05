1763753d7d4841e2c3b018233757e269f7ddc17c
RyanQuey <rlquey2@Gmail.com>
2018-02-05 08:43:52 -0800

    switch over to node lib for googleapis

--- api/services/Google.js
+++ api/services/Google.js
@@ -1,9 +1,40 @@
 const apiKey = process.env.GOOGLE_API_KEY || sails.config.env.GOOGLE_API_KEY
+const google = require('googleapis');
+const urlshortener = google.urlshortener('v1');
+google.options({
+  auth: apiKey,
+})
+
+const _setup = (account, accessToken) => {
+  const fb = new FB.Facebook({
+    //this lib automatically adds the app secret key
+    appId,
+    appSecret,
+    accessToken,
+    version: 'v2.10',
+    timeout_ms: 60*1000, //1 min
+  })
+
+  return fb
+}
+
 const Google = {
   shortenUrl: (url, options = {}) => {
     return new Promise((resolve, reject) => {
-      axios.post(`https://www.googleapis.com/urlshortener/v1/url?key=${apiKey}`, {longUrl: url})
-      .then((res) => {
+      //axios.post(`https://www.googleapis.com/urlshortener/v1/url?key=${apiKey}`, {longUrl: url})
+      urlshortener.url.post({longUrl: url}, (err, res) => {
+        if (err) {
+          console.log("error shortening URL");
+          let error = Helpers.safeDataPath(err, "response.data.error", err)
+          console.log(error);
+          if (options.alwaysResolve) {
+            return resolve(error)
+          } else {
+            return reject(error)
+          }
+
+        }
+
         const result = res.data
         //result will be like: {
         //  kind: "urlshortener#url,
@@ -12,17 +43,6 @@ const Google = {
         //}
         console.log("success", result);
         return resolve(result.id)
-      })
-      .catch((err) => {
-        console.log("error shortening URL");
-        let error = Helpers.safeDataPath(err, "response.data.error", err)
-        console.log(error);
-        if (options.alwaysResolve) {
-          return resolve(error)
-        } else {
-          return reject(error)
-        }
-
       })
     })
   },
@@ -33,8 +53,19 @@ const Google = {
         return resolve("")
       }
 
-      axios.get(`https://www.googleapis.com/urlshortener/v1/url?key=${apiKey}&shortUrl=${url}`)
-      .then((res) => {
+      //axios.get(`https://www.googleapis.com/urlshortener/v1/url?key=${apiKey}&shortUrl=${url}`)
+      urlshortener.url.get({shortUrl: url}, (err, res) => {
+        if (err) {
+          console.log("error expanding URL");
+          let error = Helpers.safeDataPath(err, "response.data.error", err)
+          console.log(error);
+          if (options.alwaysResolve) {
+            return resolve(error)
+          } else {
+            return reject(error)
+          }
+        }
+
         const result = res.data
         //result will be like: {
         //  kind: "urlshortener#url,
@@ -43,16 +74,6 @@ const Google = {
         //  status: "OK" (or "MALWARE"...if fishy)
         //}
         return resolve(result.longUrl)
-      })
-      .catch((err) => {
-        console.log("error expanding URL");
-        let error = Helpers.safeDataPath(err, "response.data.error", err)
-        console.log(error);
-        if (options.alwaysResolve) {
-          return resolve(error)
-        } else {
-          return reject(error)
-        }
       })
     })
   },
@@ -63,8 +84,22 @@ const Google = {
         return resolve({})
       }
 
-      axios.get(`https://www.googleapis.com/urlshortener/v1/url?key=${apiKey}&shortUrl=${url}&projection=FULL`)
-      .then((res) => {
+      //axios.get(`https://www.googleapis.com/urlshortener/v1/url?key=${apiKey}&shortUrl=${url}&projection=FULL`)
+      urlshortener.url.get({
+        shortUrl: url,
+        projection: "FULL",
+      }, (err, res) => {
+        if (err) {
+          console.log("error getting analytics for URL");
+          let error = Helpers.safeDataPath(err, "response.data.error", err)
+          console.log(error);
+          if (options.alwaysResolve) {
+            return resolve(error)
+          } else {
+            return reject(error)
+          }
+        }
+
         const result = res.data
         //result will be like: {
         //  kind: "urlshortener#url,
@@ -80,17 +115,6 @@ const Google = {
         //    twoHours:
         //}
         return resolve(result)
-      })
-      .catch((err) => {
-        console.log("error getting analytics for URL");
-        let error = Helpers.safeDataPath(err, "response.data.error", err)
-        console.log(error);
-        if (options.alwaysResolve) {
-          return resolve(error)
-        } else {
-          return reject(error)
-        }
-
       })
     })
   },
