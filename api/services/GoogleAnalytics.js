ba9690dfa36dbd78592b4a42865dab7b51920d32
RyanQuey <rlquey2@Gmail.com>
2018-02-06 16:01:03 -0800

    getting the basic data from Google

--- api/services/GoogleAnalytics.js
+++ api/services/GoogleAnalytics.js
@@ -42,27 +42,104 @@ const GAHelpers = {
     })
   },
 
+  generateReportRequest: (filters) => {
+    const viewId = filters.profileId
+    let dateRanges
+    if (filters.startDate) {
+      dateRanges = [{startDate: filter.startDate, endDate: filter.endDate || moment(filter.startDate).add(1, "week").format("YYYY-MM-DD")}]
+    }
+
+    // want to apply this for each report set
+
+    // various filters to be applied to the tempalte. Might also get counts for
+    /*
+    const filters = [
+      {
+        dimensionFilterClauses: [
+          {
+            dimensionName: "ga:hasSocialSourceReferral",
+            operator: "EXACT",
+            expressions: [true], //to get social referrals
+          },
+        ],
+      },
+      {
+        dimensionFilterClauses: [
+          {
+            dimensionName: "ga:medium",
+            operator: "EXACT",
+            expressions: ["(direct)"],
+          },
+        ],
+      },
+      {
+        dimensionFilterClauses: [
+          {
+            dimensionName: "ga:medium",
+            operator: "EXACT",
+            expressions: ["organic"],
+          },
+        ],
+      },
+      {
+        dimensionFilterClauses: [], //this would be to get the total
+      },
+    ]
+    */
+
+    const reportRequest = {
+      viewId,
+      dateRanges,
+      metrics: [
+        {expression: "ga:pageviews"},
+      ], //sessions, users, unique page views, pageviews per session and more are also valid things to default to
+      dimensions: [
+        {name: "ga:pagePath"}, // could also do pageTitle? or just do both?
+        {name: "ga:pageTitle"},
+        {name: "ga:segment"},
+        {name: "ga:fullReferrer"}, //full url of referring webpage, if exists
+        {name: "ga:hasSocialSourceReferral"}, // will extract this to get social referral traffic
+      ],
+      segments: [
+        {segmentId: "gaid::-5"}, //organic traffic
+        {segmentId: "gaid::-7"}, //direct traffic
+        {segmentId: "gaid::-7"}, //direct traffic
+        {segmentId: "gaid::-1"}, //should be total
+        /*{
+          dynamicSegment: {
+            name: "Traffic from Social Site",
+            userSegment: {
+              segmentFilters: [{
+                simpleSegment: {
+                  orFiltersForSegment: [{
+                    segmentFilterClauses: [{
+                      dimensionFilter:{
+                        dimensionName: "ga:hasSocialSourceReferral", //can't use in segment, is banned
+                        operator: "EXACT",
+                        expressions: [true],
+                      }
+                    }]
+                  }]
+                }
+              }]
+            }
+          }
+        },*/
+      ]
+    }
+
+    return reportRequest
+  },
+
   getReport: (providerAccount, filters) => {
     return new Promise((resolve, reject) => {
       let analyticsAccounts, currentAnalyticsAccount
-      const reportSets = filters.reportSets || [{
-        metrics: [{expression: "ga:organicSearches"}],
-        //dimensions: [{name: "ga"}]
-      }]
       const oauthClient = Google._setup(providerAccount)
      //all requests should have the same daterange, viewId, segments, samplingLevel, and cohortGroup (these latter ones are a TODO)
-      const reqTemplate = {
-        viewId: filters.profileId,
-      }
-      //TODO haven't tested
-      if (filters.startDate) {
-        reqTempate.dateRanges = [{startDate: filter.startDate, endDate: filter.endDate || moment(filter.startDate).add(1, "week").format("YYYY-MM-DD")}]
-      }
+      const reportRequest = GoogleAnalytics.generateReportRequest(filters)
 
       //NOTE: can do max of 5
-      const reportRequests = reportSets.map((set) =>
-        Object.assign({}, reqTemplate, set)
-      )
+      const reportRequests = [reportRequest]
 
       const params = {
         auth: oauthClient,
